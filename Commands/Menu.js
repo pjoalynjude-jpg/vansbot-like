167e6a3f32e366b9b74db912787e053c8c0f8e3f
pjoalynjude-jpg <pjoalynjude@gmail.com>
2026-02-20 16:40:23 -0500

    Refactor menu command to use a variable for message

--- Commands/Menu.js
+++ Commands/Menu.js
@@ -4,18 +4,35 @@ export default {
   name: "menu",
   description: "Afficher le menu du bot",
   execute(sock, msg) {
-    sock.sendMessage(msg.key.remoteJid, {
-      text: `
+    const menu = `
 🤖 *${config.botName}*
-━━━━━━━━━━━━━━
+━━━━━━━━━━━━━━━
+📌 *COMMANDES DE BASE*
 ${config.prefix}menu
 ${config.prefix}ping
 ${config.prefix}help [commande]
+
+👥 *GROUPES*
+${config.prefix}welcome
+${config.prefix}goodbye
 ${config.prefix}tagall [message]
+
+🌐 *MÉDIAS*
+${config.prefix}youtube / yt [lien]
+${config.prefix}tiktok [lien]
+${config.prefix}search [recherche]
+
+🤖 *INTELLIGENCE*
+${config.prefix}ia [question]
+
+⚙️ *PARAMÈTRES*
 ${config.prefix}setprefix [préfixe]
-━━━━━━━━━━━━━━
-Owner : ${config.owner}
+${config.prefix}setprivate
+${config.prefix}setpublic
+━━━━━━━━━━━━━━━
+👑 Owner : ${config.owner}
+🔐 Mode : ${config.mode.toUpperCase()}
 `
-    })
+    sock.sendMessage(msg.key.remoteJid, { text: menu })
   }
-}
+  }
