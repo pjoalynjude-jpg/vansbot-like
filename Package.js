cca8fa435391a341e04799e0f95fa642dcca0485
pjoalynjude-jpg <pjoalynjude@gmail.com>
2026-02-19 21:17:47 -0500

    Initialize Package.js for WhatsApp bot project

---
+++ Package.js
@@ -0,0 +1,14 @@
+{
+  "name": "vansbot-like",
+  "version": "1.0.0",
+  "description": "Bot WhatsApp style VansBot (Baileys)",
+  "type": "module",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "@whiskeysockets/baileys": "^6.7.0",
+    "qrcode-terminal": "^0.12.0"
+  }
+    }
