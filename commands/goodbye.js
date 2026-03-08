25b0ae194c67f3308669237dd3bd3576c368f927
pjoalynjude-jpg <pjoalynjude@gmail.com>
2026-03-07 21:13:43 -0500

    Update goodbye.js

--- commands/goodbye.js
+++ commands/goodbye.js
@@ -1,26 +1,4 @@
-const fs = require("fs")
-
-module.exports = {
-  name: "goodbye",
-  execute(sock, msg, args) {
-    const settings = JSON.parse(fs.readFileSync("./settings.json"))
-
-    if (!args[0]) {
-      return sock.sendMessage(msg.key.remoteJid, {
-        text: "Utilise : !goodbye on / off"
-      })
-    }
-
-    if (args[0] === "on") {
-      settings.goodbye = true
-      fs.writeFileSync("./settings.json", JSON.stringify(settings, null, 2))
-      return sock.sendMessage(msg.key.remoteJid, { text: "✅ Goodbye activé" })
-    }
-
-    if (args[0] === "off") {
-      settings.goodbye = false
-      fs.writeFileSync("./settings.json", JSON.stringify(settings, null, 2))
-      return sock.sendMessage(msg.key.remoteJid, { text: "❌ Goodbye désactivé" })
-    }
-  }
-        }
+export default async (sock, from, state) => {
+  state.goodbye = !state.goodbye
+  sock.sendMessage(from, { text: `👋 Goodbye: ${state.goodbye}` })
+}
