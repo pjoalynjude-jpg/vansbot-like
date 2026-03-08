3f8c7e37d9e9bad2b937201c2a62d4d63d1f385d
pjoalynjude-jpg <pjoalynjude@gmail.com>
2026-03-07 21:19:09 -0500

    Update welcome.js

--- commands/welcome.js
+++ commands/welcome.js
@@ -1,26 +1,4 @@
-const fs = require("fs")
-
-module.exports = {
-  name: "welcome",
-  execute(sock, msg, args) {
-    const settings = JSON.parse(fs.readFileSync("./settings.json"))
-
-    if (!args[0]) {
-      return sock.sendMessage(msg.key.remoteJid, {
-        text: "Utilise : !welcome on / off"
-      })
-    }
-
-    if (args[0] === "on") {
-      settings.welcome = true
-      fs.writeFileSync("./settings.json", JSON.stringify(settings, null, 2))
-      return sock.sendMessage(msg.key.remoteJid, { text: "✅ Welcome activé" })
-    }
-
-    if (args[0] === "off") {
-      settings.welcome = false
-      fs.writeFileSync("./settings.json", JSON.stringify(settings, null, 2))
-      return sock.sendMessage(msg.key.remoteJid, { text: "❌ Welcome désactivé" })
-    }
-  }
-        }
+export default async (sock, from, state) => {
+  state.welcome = !state.welcome
+  sock.sendMessage(from, { text: `👋 Welcome: ${state.welcome}` })
+}
